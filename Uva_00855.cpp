c26dea743767c0ceab433f52abbbda41a11dcb0f
MeA1409 <33351694+MeA1409@users.noreply.github.com>
2018-03-24 18:13:00 +0700

    Create Uva_00855.cpp

---
+++ Uva_00855.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    int s, a, f;
+    while (t--) {
+        cin >> s >> a >> f;
+        vector <int> streets(f), avenues(f);
+        for (int i = 0; i < f; i++) 
+            cin >> streets[i] >> avenues[i];
+        sort(streets.begin(), streets.end());
+        sort(avenues.begin(), avenues.end());
+        int n = f/2;
+        if (f%2==0) n--;
+        cout << "(Street: " << streets[n] << ", Avenue: " << avenues[n] << ")\n";
+    }
+}
